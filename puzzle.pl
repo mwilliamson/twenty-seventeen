0784395dad1d3d3f037c12580ac5c589ff0ce2a9
Michael Williamson <mike@zwobble.org>
2017-01-05 19:24:02 +0000

    Print solution

--- puzzle.pl
+++ puzzle.pl
@@ -1,10 +1,22 @@
-puzzle([X], X).
-puzzle(List, X) :-
-    select(List, Before, First, Second, After),
+puzzle(List, X, S) :-
+    indexed(List, IndexedList),
+    puzzle2(IndexedList, X, S).
+
+indexed(List, IndexedList) :- indexed(List, IndexedList, 0).
+
+indexed([], [], _).
+indexed([Head|Tail], [(Index, Head)|IndexedTail], Index) :-
+    NextIndex is Index + 1,
+    indexed(Tail, IndexedTail, NextIndex).
+
+puzzle2([(_, X)], X, []).
+puzzle2(List, X, [(FirstIndex, First, Second, Op)|S]) :-
+    select(List, Before, (FirstIndex, First), (_, Second), After),
     operation(Op),
     perform_operation(Op, First, Second, OpResult),
-    append(Before, [OpResult|After], Next),
-    puzzle(Next, X).
+    append(Before, [(FirstIndex, OpResult)|After], Next),
+    puzzle2(Next, X, S).
+
 
 select(List, Before, First, Second, After) :-
     append(Before, [First, Second|After], List).
@@ -19,6 +31,6 @@ perform_operation(subtract, X, Y, Z) :- Z is X - Y.
 perform_operation(times, X, Y, Z) :- Z is X * Y.
 %perform_operation(divide, X, Y, Z) :- Z is X + Y.
 
-:- puzzle([1], 1).
-:- puzzle([3, 2], 1).
-:- puzzle([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 2017).
+:- puzzle([1], 1, _).
+:- puzzle([3, 2], 1, _).
+:- puzzle([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 2017, S), print(S).
